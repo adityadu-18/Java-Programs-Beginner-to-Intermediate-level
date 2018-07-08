17dfcd262050c266a234dbf433387b720880ff71
Aravind V. Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-07-08 21:04:29 +0530

    Update Addition of 2 numbers.java

--- Source Codes/Addition of 2 numbers.java
+++ Source Codes/Addition of 2 numbers.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 public class Addition {
-		
 	public static void main(String[] args) {
-			
 		Scanner readme=new Scanner(System.in);
-			System.out.println("Enter 2 numbers");
-			double n1,n2,sum;
-			n1=readme.nextDouble();
-			n2=readme.nextDouble();
-			sum=n1+n2;
-			System.out.println("Sum = "+sum);
-			
-		}
+		System.out.println("Enter 2 numbers: ");
+		double n1, n2, sum;
+		n1 = readme.nextDouble();
+		n2 = readme.nextDouble();
+		sum = n1 + n2;
+		System.out.println("Sum = " + sum);	
+	}
 }
